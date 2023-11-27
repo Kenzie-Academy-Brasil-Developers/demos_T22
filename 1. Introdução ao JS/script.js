2fc8e303b4f8ab51c6c0909c31fb93f4710f9220
alexandersilvadev <104785550+alexandersilvadev@users.noreply.github.com>
2023-11-27 11:46:19 -0300

    Add files via upload

---
+++ 1. Introdução ao JS/script.js
@@ -0,0 +1,66 @@
+console.log("Olá turma 22")
+console.log(10);
+
+// Sintaxe ao declarar uma variável:
+// camelCase x snake_case
+
+// let 1nome = "não existe"
+// let nome1 = "não existe"
+// let nomeReal = ""
+
+// let idade_verdadeira = "" // Python
+
+// Variáveis
+
+// let (pode reatribuir o valor)
+
+// Declarar Variável - let
+
+let nome = "Alexander";
+console.log(nome);
+
+// Reatribuição de valor de variável
+
+nome = "Alex";
+console.log(nome);
+
+// const (NÃO pode reatribuir o valor)
+
+const cpf = "111111111111";
+console.log(cpf); 
+
+// cpf = "22222222222"
+
+// var (NÃO USAMOS MAIS)
+
+// var idade = 19 - VAR NÃO!
+// console.log(idade)
+
+// Tipos de variáveis
+// 1 - Inteiro (number)
+let idadeReal = 20;
+console.log(idadeReal);
+idadeReal = 21;
+console.log(idadeReal);
+
+// 2 - Float (number - ponto flutuante)
+let peso = 75.5000001;
+let peso2 = 75.5;
+console.log(peso);
+console.log(peso2);
+
+// 3 - Texto (string - aspas simples, duplas ou crase)
+let sobrenome = `Silva Sakaki`;
+let texto =
+    "Javascript é apenas mais uma das diversas linguagens de programação.";
+console.log(sobrenome);
+console.log(texto);
+
+// 4 - Boleano (boolean)
+let idadeVerdadeira = true;
+console.log(idadeVerdadeira);
+idadeVerdadeira = false;
+console.log(idadeVerdadeira);
+
+let pesoVerdadeiro = false;
+console.log(pesoVerdadeiro);
