b8813ca582713154d5aaf221647f18cbffcc9813
alexandersilvadev <104785550+alexandersilvadev@users.noreply.github.com>
2023-11-28 09:42:56 -0300

    Add files via upload

---
+++ 1. Introdução ao JS - Noite/script.js
@@ -0,0 +1,79 @@
+console.log("Olá t22. Bem vindos ao m2")
+
+// alert("Olá usuário!")
+// alert("Bem vindo ao M2")
+
+// Variáveis
+// tipo nome da variável = valor da variável
+
+// Declarando uma variável
+let qualquer = "Alexander"
+console.log(qualquer)
+
+// Redeclarando o valor da variável
+
+qualquer = "Alex"
+console.log(qualquer)
+
+// const
+const idade = 20
+console.log(idade)
+
+// Não posso alterar o valor!
+// idade = "Vinte e um"
+
+// var
+
+// NÃO UTILIZAMOS MAIS!
+
+sobrenome = "Sakaki"
+console.log(sobrenome)
+
+sobrenome = "Silva"
+console.log(sobrenome)
+
+
+// Valores dos tipos de variávies
+
+// tipo numero (number)
+// let numero1 = 10
+// let numero2 = 5
+
+let total = 10 + 5
+console.log(total)
+
+// tipo float (number)
+let outro = 78.10
+let flutuante = 75.500
+let peso = 75.500000000001 + 4
+
+console.log(peso)
+// tipo texto (string)
+let nome = "Alex"
+let texto = "Um algoritmo é uma sequência finita de ações executáveis que visam         obter uma solução para um determinado tipo de problema."
+console.log(texto)
+
+// tipo boleano (boolean)
+let verdadeiro = true
+let falso = false
+
+console.log(verdadeiro)
+console.log(falso)
+
+// camelCase 
+
+// snake_case
+
+// let 1numero = 10 // Não pode
+// let numero1 = 10 // Posso declarar
+// let num2dois = 2 // Posso declarar
+
+// let nome-sobrenome = "" // Não pode
+
+let nomeSobrenome = ""
+
+// let nome_sobrenome = "" // python utiliza snake_case
+
+
+
+
