7a6c75eef4220e98e3afd6b0d171c313da58c30a
alexandersilvadev <104785550+alexandersilvadev@users.noreply.github.com>
2023-11-29 13:46:15 -0300

    Add files via upload

---
+++ 2. Operadores de comparação/script.js
@@ -0,0 +1,66 @@
+// let, const e var (não utilizar var)
+// number, string e boolean
+
+// let nome = "fulano";
+// let sobrenome = "Ciclano"
+// let modulo = "M2"
+
+// // Concatenação de string
+
+// // let mensagem = "Bem vindo " + nome + "!";
+// let mensagem = "Bem vindo " + nome + " " + sobrenome + ", você está no modulo " + modulo + "!"
+
+// // Interpolação de string / template string
+
+// let mensagem2 = `Bem vindo ${nome}, você está no modulo ${modulo}!`
+
+// // let nomeCompleto = nome + sobrenome
+
+// console.log(mensagem);
+// console.log(mensagem2);
+
+// Indices de uma string
+//              0 1 2 3
+// let nickName = "Alex"
+
+// console.log(nickName.length)
+// console.log(nickName[4])
+
+// Prompt e alert
+
+
+// alert("Olá")
+// let nome = prompt("Digite seu nome: ")
+// let sobrenome = prompt("Digite seu sobrenome: ")
+// let modulo = prompt("Digite o seu módulo: ")
+
+
+// alert(`Bem vindo ${nome} ${sobrenome}, você está no modulo M${modulo}!`)
+
+// Comparadores
+// igualdade == ou === (compara tipo e valores)
+// diferente != ou !== (compara tipo e valores)
+
+// maior e maior ou igual| > ou >=
+// menor e menor ou igual| < ou <= 
+
+// Condicionais
+// if e else
+// Se o que tiver dentro dos parêntesis for verdadeiro, execute o que tiver entre chaves.
+// Se o que tiver dentro dos parêntesis for falso, não faça nada entre as chaves
+
+// let nome = prompt("Digite seu nome: ")
+
+// if (nome == "Alex") {
+//     // Execute isso:
+//     alert("Bem vindo instrutor " + nome)
+// } 
+// else if (nome == "Fulano") {
+//     alert("Bem vindo aluno " + nome)
+// }
+// else {
+//     // se não, execute isso:
+//     alert("Você não é bem vindo, cara!")
+// }
+
+// console.log("Depois da estrutura de if e else")
