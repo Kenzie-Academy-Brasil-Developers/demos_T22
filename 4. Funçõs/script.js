53e59119871ad5a9f738958d70ba6049a2554d63
alexandersilvadev <104785550+alexandersilvadev@users.noreply.github.com>
2023-12-04 11:18:24 -0300

    Add files via upload

---
+++ 4. Funçõs/script.js
@@ -0,0 +1,82 @@
+// Operadores de incremento e decremento (++) (--)
+// Condicionais (if, else e else if)
+// Operadores lógicos (&&) (||) (!)
+
+// Funções
+
+// Sintaxe para declarar uma função:
+
+// Função sem parâmetro e sem retorno
+
+// palavra   nome da função parametros e escopo
+// function nomeDaFuncao () {
+//     // bloco de código ou condicionais...
+//     console.log("Dentro da função")
+// }
+
+// Função com parâmetro e sem retorno
+// O que vai dentro dos parêntesis na declaração, se chama PARAMETRO
+// function mensagem (nome, modulo) {
+//     console.log(`Olá ${nome}. Bem vindo ao ${modulo}`)
+// }
+
+// O que vai dentro do parêntesis quando eu chamo a função, se chama ARGUMENTO
+
+// let nome = "Fulano"
+// let moduloDeEstudo = "M2"
+// mensagem(nome, moduloDeEstudo)
+
+// Função sem parâmetro e com retorno
+
+// function mensagemDeBoasVindas () {
+//     let nome = prompt("Digite seu nome: ")
+//     return `Olá ${nome}. Bem vindo!`
+// }
+
+// function boleano () {
+//     return true
+// }
+
+// // let retornoDaFuncao = mensagemDeBoasVindas()
+// console.log(boleano())
+
+// Função com parâmetro e com retorno
+
+// function soma (num1, num2) {
+//     let aoQuadrado = num1**2
+//     let dividido = num2 / 2
+//     // let total = num1 + num2
+//     // return total
+//     return aoQuadrado + dividido
+// }
+
+// console.log(soma(4, 10));
+
+// function soma(num1, num2) {
+//     return num1 + num2;
+// }
+
+// function subtrair(num1, num2) {
+//     return num1 - num2;
+// }
+
+// if (soma(10, 9) < 18) {
+//     console.log("Entrou no if")
+// }
+
+// let numero = 10
+
+// function verificaPar(num) {
+//     if (num % 2 == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// let idade = prompt("Digite sua idade: ")
+
+// if (verificaPar(37)) {
+//     console.log("Sua idade é par")
+// }
+
